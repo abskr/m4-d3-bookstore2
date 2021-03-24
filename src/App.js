1bccf149edaaba315b798f80eb371ef382663396
ABskr <bskr26@gmx.de>
2021-03-24 15:28:00 +0100

    second Commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,14 @@ import "./App.css";
 import WarningSign from "./components/WarningSign";
 import MyBadge from "./components/MyBadge";
 import "bootstrap/dist/css/bootstrap.min.css";
-import SingleBook from "./components/SingleBook";
+import BookList from "./components/BookList"
 
 function App() {
   return (
     <div>
       <WarningSign text="This is some random text" />
       <MyBadge text="This is the badge text" color="success" />
-      {/* <SingleBook
-        book={{
-          asin: "0316438960",
-          title: "The Last Wish: Introducing the Witcher",
-          img:
-            "https://images-na.ssl-images-amazon.com/images/I/51eHtkVLL5L.jpg",
-          price: 9.59,
-          category: "fantasy",
-        }}
-      /> */}
+      <BookList />
     </div>
   );
 }
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,11 +1,12 @@
 import { Card, Button } from "react-bootstrap";
+import EllipsisText from "react-ellipsis-text";
 
 const SingleBook = (props) => {
   return (
     <Card>
       <Card.Img variant="top" src={props.book.img} height="300px" />
       <Card.Body>
-        <Card.Title>{props.book.title}</Card.Title>
+        <Card.Title>{props.book.title.slice(0,15)} ...</Card.Title>
         <Card.Text>{props.book.price}</Card.Text>
         <Button variant="success">Add to Cart</Button>
       </Card.Body>
